84813668f924a1a6a056c6ffa642e5d053a845dc
Negar Jamalifard <n.jamalifard@gmail.com>
2018-02-18 01:54:48 +0330

    data binding and passing data from outside to the single page

---
+++ vuebnb-prototype/app.js
@@ -0,0 +1,15 @@
+
+let app = new Vue({
+	el: "#app",
+	data:{
+		title: sample.title,
+		address: sample.address,
+		about: sample.about,
+		headerImageStyle: {
+			'background-image': 'url(sample/header.jpg)'
+		},
+		amenities: sample.amenities,
+		prices: sample.prices
+
+	}
+});
